dfae53e651d07d99d09112c90c262be9c6fb1dfb
mnicolas94 <miguelcok27@gmail.com>
2022-09-16 11:22:00 -0400

    FEAT: draw foldout

--- Editor/FiltersPredicatePropertyDrawer.cs
+++ Editor/FiltersPredicatePropertyDrawer.cs
@@ -9,18 +9,35 @@ namespace SerializablePredicate.Editor
     {
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            var targetHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("_target"));
-            var filtersHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("_filters"));
-            return targetHeight + filtersHeight;
+            float height;
+            if (property.isExpanded)
+            {
+                var targetHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("_target"));
+                var filtersHeight = EditorGUI.GetPropertyHeight(property.FindPropertyRelative("_filters"));
+                height = targetHeight + filtersHeight;
+            }
+            else
+            {
+                height = base.GetPropertyHeight(property, label);
+            }
+
+            return height;
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            var labelText = label.text;
+            var fixedLabel = new GUIContent(labelText);
+            var foldoutRect = new Rect(position)
+            {
+                height = base.GetPropertyHeight(property, label)
+            };
+            property.isExpanded = EditorGUI.Foldout(foldoutRect, property.isExpanded, label);
+
             var target = property.FindPropertyRelative("_target");
             var filters = property.FindPropertyRelative("_filters");
             var targetHeight = EditorGUI.GetPropertyHeight(target);
             var filtersHeight = EditorGUI.GetPropertyHeight(filters);
-            
             var targetRect = position;
             targetRect.height = targetHeight;
             var filtersRect = targetRect;
@@ -28,7 +45,7 @@ namespace SerializablePredicate.Editor
             filtersRect.height = filtersHeight;
 
             EditorGUI.BeginChangeCheck();
-            EditorGUI.PropertyField(targetRect, target);
+            EditorGUI.PropertyField(targetRect, target, new GUIContent(labelText));
             bool changed = EditorGUI.EndChangeCheck();
             if (changed)
             {
@@ -36,9 +53,12 @@ namespace SerializablePredicate.Editor
                 predicate.Editor_UpdateFiltersType();
             }
             
-            EditorGUI.indentLevel++;
-            EditorGUI.PropertyField(filtersRect, filters);
-            EditorGUI.indentLevel--;
+            if (property.isExpanded)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUI.PropertyField(filtersRect, filters);
+                EditorGUI.indentLevel--;
+            }
         }
     }
 }
